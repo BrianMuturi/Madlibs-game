2dd8c460336ade5493fa40ee98bb76882fba0210
Brian Muturi <muturibrian18@gmail.com>
2020-12-23 02:54:35 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Madlibs_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Color:");
+            string color = Console.ReadLine();
+            Console.Write("Plural noun:");
+            string noun = Console.ReadLine();
+            Console.Write("Celebrity:");
+            string celebrity = Console.ReadLine();
+            Console.WriteLine("Roses are "+ color);
+            Console.WriteLine(noun + " are blue");
+            Console.WriteLine("I love " + celebrity);
+
+            Console.ReadLine();
+        }
+    }
+}
